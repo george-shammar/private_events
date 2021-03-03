2eb10aad9b0e3fbcc063e67fb096c447705a3689
George <george.gbenle@outlook.com>
2021-03-03 15:38:17 +0000

    navigation bar with current user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+    def navbar_button_name(member)
+        link_to user.name, '/',  class: 'btn btn-light mx-1' if user_signed_in?
+    end
+    
+    def navbar_button_new_post
+        link_to 'New Event', new_event_path, class: 'btn btn-primary mx-1' if user_signed_in?
+    end
+    
+    def navbar_button_log_out
+        link_to 'Log out', logout_path, method: :delete, class: 'btn btn-primary mx-1' if user_signed_in?
+    end
+    
+    def navbar_button_sign_up
+        link_to 'Sign up', new_user_path, class: 'btn btn-primary mx-1' unless user_signed_in?
+    end
+    
+    def navbar_button_log_in
+        link_to 'Sign in',login_path, class: 'btn btn-primary mx-1' unless user_signed_in?
+    end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def user_signed_in?
+        !current_user.nil?
+    end
 end
