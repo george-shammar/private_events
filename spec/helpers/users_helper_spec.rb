c1e698e114211d4604e8b31911f97ff4c5b4ce6c
aliciapaz <aliciapazrojas@gmail.com>
2021-03-04 17:13:48 -0300

    add test unit to users_helper

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 include UsersHelper
 RSpec.describe UsersHelper, type: :helper do
-  it 'retrieves the events created by a user' do
-    @user = User.create(name: 'Foo', email: 'foo@bar.com', password: '123456789')
-    @event = @user.events.build(title: 'Birthday', location: 'Foo City', date: '2021-04-02', time: "2000-01-01 14:22:00.000000000 +0000")
-    expect(find_events_by_user(@user).last[:title]).to equal('Birthday')
+  creator = User.first_or_create!(name: 'Foo', email: 'foo@bar.com', password: '123456789')
+  @event = creator.events.build(title: 'Birthday', location: 'Foo City', date: '2021-04-02', time: "2000-01-01 14:22:00.000000000 +0000")
+  it 'retrieves all the events created by a user' do
+    events = find_events_by_user(creator)
+    expect(events.all? { |event| event.user === creator }).to be true
     end
+
 end
