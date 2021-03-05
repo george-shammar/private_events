b15913a69d0c8a0f4b3d3bef032e1bb2c49f660c
aliciapaz <aliciapazrojas@gmail.com>
2021-03-05 11:31:48 -0300

    fix helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def list_events events
-    content_tag :ul do
+    content_tag :ul, class: 'd-flex flex-column' do
       events.collect { |event| concat link_to event[0], event_path(event[1])  }
     end
   end
