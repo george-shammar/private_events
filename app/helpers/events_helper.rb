2c19b9fb9dd213846dd43638420ee2be40dd69f8
aliciapaz <aliciapazrojas@gmail.com>
2021-03-09 13:22:00 -0300

    fix events#show page

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -18,5 +18,15 @@ module EventsHelper
       end
     end 
   end
+
+  def show_invitees invitees
+    unless @invitees.empty?
+      content_tag :ul, class: 'd-flex flex-column' do
+      invitees.collect { |invitee| concat(content_tag(:li, invitee)) }
+    end
+    else
+    "There are no invitees yet"
+    end
+  end 
  
 end
