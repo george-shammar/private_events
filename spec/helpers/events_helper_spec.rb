5eb211c5787502fd8c162e34506903b4424879f7
George <george.gbenle@outlook.com>
2021-03-09 22:49:36 +0000

    Create test for event helper show page

--- spec/helpers/events_helper_spec.rb
+++ spec/helpers/events_helper_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the EventsHelper. For example:
-#
-# describe EventsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe EventsHelper, type: :system do
   describe 'index page' do
     it 'shows a list of past events' do
@@ -25,5 +15,16 @@ RSpec.describe EventsHelper, type: :system do
       visit root_path
       expect(page).to have_content('Birthday')
     end
+  end
+
+  describe 'show page' do 
+    it 'shows a list of invitees to an event' do 
+      @invitee = User.first_or_create!(name: 'Foo', email: 'foo@bar.com', password: '123456789')
+    @birthday = Event.create(title: 'Birthday', location: 'Foo City', date: '2021-04-02',
+      time: '2000-01-01 14:22:00.000000000 +0000', description: 'Birthday', creator_id: @invitee.id)
+    @invitation = Invitation.create(invitee_id: @invitee.id, event_id: @birthday.id) 
+     visit event_path(@birthday.id)
+     expect(page).to have_content('Foo')
+  end
   end
 end
