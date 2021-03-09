ab4a88a7a88cb863600bb72f396de126d71975c1
aliciapaz <aliciapazrojas@gmail.com>
2021-03-09 19:17:48 -0300

    add event model tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'finds all upcoming events' do
+    @birthday = Event.create(title: 'Birthday', location: 'Foo City', date: '2022-04-02',
+      time: '2000-01-01 14:22:00.000000000 +0000', description: 'Birthday', creator_id: 1)
+    upcoming = Event.find_upcoming_events
+    expect(upcoming.last[0]).to eq('Birthday')
+    end
+
+    it 'finds all past events' do
+      @birthday = Event.create(title: 'Birthday', location: 'Foo City', date: '2020-04-02',
+        time: '2000-01-01 14:22:00.000000000 +0000', description: 'Birthday', creator_id: 1)
+      past = Event.find_past_events
+      expect(past.last[0]).to eq('Birthday')
+      end
 end
