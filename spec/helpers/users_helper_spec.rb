75ecdbdb44d0f2c39510da64657d5c8f07eb51fc
aliciapaz <aliciapazrojas@gmail.com>
2021-03-10 12:27:35 -0300

    add users_helper tests

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe UsersHelper, type: :helper do
+RSpec.describe UsersHelper, type: :system do
+  describe 'users show page' do
+    it 'shows a list of created events' do
+      @user = User.first_or_create!(name: 'Foo', email: 'foo@bar.com', password: '123456789')
+      @birthday = Event.create(title: 'Birthday', location: 'Foo City', date: '2020-04-02',
+          time: '2000-01-01 14:22:00.000000000 +0000', description: 'Birthday', creator_id: 1)
+      visit event_path(@user.id)
+    expect(page).to have_content('Birthday')
+    end
+
+    it 'shows a list of invitations' do
+      @creator = User.first_or_create!(name: 'Fee', email: 'fee@bar.com', password: '123456789')
+      @invitee = User.create(name: 'Foo', email: 'foo@bar.com', password: '123456789')
+      @birthday = Event.create(title: 'Birthday', location: 'Foo City', date: '2020-04-02',
+          time: '2000-01-01 14:22:00.000000000 +0000', description: 'Birthday', creator_id: @creator.id)
+      @invitation = Invitation.create(invitee_id: @invitee.id, event_id: @birthday.id) 
+      visit user_path(@invitee.id)
+      expect(page).to have_content('Birthday')
+    end
     
+  end    
 end
+
+
+# list_invitations 
